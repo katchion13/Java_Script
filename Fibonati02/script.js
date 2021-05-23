8a3c3665ca0a4d9be02609afb7fb0042867c45df
katchion13 <69910600+katchion13@users.noreply.github.com>
2021-05-23 16:53:09 +0900

    Add files via upload

---
+++ Fibonati02/script.js
@@ -0,0 +1,19 @@
+function fibo(){
+ var termo = parseInt(document.getElementById('numero').value);
+  var resposta = document.getElementById('resposta');
+  var penultimo=0, ultimo=1;
+  var numero;
+	document.write("<b>WhileとLoopsを使ったフィボナッチ </b><br />");
+	var penultimo=0, ultimo=1;
+        var numero;
+		document.write(termo + "より数字が低いフィボナッチ数列"+"<br />");
+		while(penultimo<termo)
+		{
+			document.write(penultimo + "<br />");
+			  numero = ultimo + penultimo;
+                          penultimo = ultimo;
+                          ultimo = numero;
+		}
+   
+  resposta.innerHTML=numero;
+}
