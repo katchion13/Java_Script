07f80466f0dd125bc52919aa50efa2186d5fecbf
Katia Midori <69910600+katchion13@users.noreply.github.com>
2021-06-13 01:05:42 +0900

    Update script_calc.js

--- Box_value01/script_calc.js
+++ Box_value01/script_calc.js
@@ -1,1 +1,16 @@
 
+document.addEventListener("DOMContentLoaded", function(){ // aguarda o DOM carregar
+
+   var sel = document.querySelector("select[name='papel']"); // seleciona o SELECT
+   var qtd = document.querySelector("input[name='quantidade']"); // seleciona o INPUT quantidade
+   sel.onchange = multi; // evento change do SELECT
+   qtd.oninput = multi; // evento oninput do INPUT quantidade
+
+   function multi(){
+      var papel = parseFloat(sel.value.replace(",","."));
+      var quant = qtd.value; // valor do INPUT quantidade
+      var res = papel * quant; // faz a multiplicação
+      document.querySelector("input[name='valor']").value = !isNaN(res) ? res.toFixed(2).replace(".", ",") : 0;
+   }
+
+});
