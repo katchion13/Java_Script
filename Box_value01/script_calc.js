f0543f0073495efb16cd9e17198b919f8268ca32
Katia Midori <69910600+katchion13@users.noreply.github.com>
2021-06-12 22:44:27 +0900

    Add files via upload

--- Box_value01/script_calc.js
+++ Box_value01/script_calc.js
@@ -1,37 +1,10 @@
-  function calculate() {
-            try
-            {
-            var value1 = parseInt(validate(document.getElementById("value1").value.trim()));
-            var value2 = parseInt(validate(document.getElementById("value2").value.trim()));
-            document.getElementById("result").value = operate(value1, value2);
-            }
-            catch (err) {
-                alert("There was a problem: " + err.messaqge);
-            }
-        }
-            catch (err) {
-                alert("There was a problem: " + err.messaqge);
-            }
-        }
+ function onButtonClick() {
+      target = document.getElementById("output");
+      target.innerHTML ="";
 
-        function operate(value1, value2) {
-            if (operator == addition) {
-                return value1 + value2;
-            } else if (operator == subtraction) {
-                return value1 - value2;
-            } else if (operator == division) {
-                return value1 / value2;
-            } else if (operator == multiplication) {
-                return value1 * value2;
-            }
-
-            function validate(value) {
-                if (value == null || value == "") {
-                    alert("Required Field");
-                    return 0;
-                } else if (isNaN(value)) {
-                    alert("Must be a Number Field");
-                    return 0;
-                } else return value;
-            }
+      for (i = 0; i < document.form1.Select1.options.length; i++) {
+        if (document.form1.Select1.options[i].selected == true) {
+          target.innerHTML += document.form1.Select1.options[i].text + "が選択されています。<br/>";
         }
+      }
+    }
