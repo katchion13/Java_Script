4678938eda89d9609aa1388e594ca2ed164019de
Katia Midori <69910600+katchion13@users.noreply.github.com>
2021-06-13 02:01:58 +0900

    Update script_calc.js

--- Box_value01/script_calc.js
+++ Box_value01/script_calc.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function(){ // aguarda o DOM carre
       var papel = papel.value;
       var quant = qtd.value; // valor do INPUT quantidade
       var res = papel * quant; // faz a multiplicação
-      document.querySelector("input[name='valor']").value = !isNaN(res) ? res.toFixed(2).replace(".", ",") : 0;
+      document.querySelector("input[name='valor']");
    }
 
 });
