d6b24d2e85281bcfc645906039a8df3b480b00f9
Katia Midori <69910600+katchion13@users.noreply.github.com>
2021-06-13 00:55:09 +0900

    Add files via upload

---
+++ Box_value01/script_calc01.js
@@ -0,0 +1,21 @@
+  function onButtonClick() {
+      selindex = document.form1.Select1.selectedIndex;
+      target = document.getElementById("output");
+      switch (selindex) {
+        case 0:
+          target.innerHTML = "100円<br/>";
+          break;
+        case 1:
+          target.innerHTML = "110円<br/>";
+          break;
+        case 2:
+          target.innerHTML = "130円<br/>";
+          break;
+        case 3:
+          target.innerHTML = "140円<br/>";
+          break;
+        case 4:
+          target.innerHTML = "150円<br/>";
+          break;
+      }
+    }
