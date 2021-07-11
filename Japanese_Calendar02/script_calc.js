296ed5e4c5cc7078cd9a0f32e7dec3107a956866
Katia Midori <69910600+katchion13@users.noreply.github.com>
2021-07-12 01:43:12 +0900

    Update script_calc.js

--- Japanese_Calendar02/script_calc.js
+++ Japanese_Calendar02/script_calc.js
@@ -2,14 +2,31 @@ document.addEventListener("DOMContentLoaded", function(){ // aguarda o DOM carre
 
    var sel = document.querySelector("select[name='papel']"); // seleciona o SELECT
    var qtd = document.querySelector("input[name='quantidade']"); // seleciona o INPUT quantidade
-   sel.onchange = multi; // evento change do SELECT
-   qtd.oninput = multi; // evento oninput do INPUT quantidade
+   sel.onchange = result; // evento change do SELECT
+   qtd.oninput = result; // evento oninput do INPUT quantidade
 
-   function multi(){
-      var papel = sel.value;
-      var quant = qtd.value; // valor do INPUT quantidade
-      var res = papel * quant; // faz a multiplicação
-      document.querySelector("input[name='valor']").value = !isNaN(res) ? res.toFixed(0).replace() : 0;
-   }
+   function passCheck(result){
+  if (result > 80){
+    if (result > 90){
+      console.log('合格です');
+      console.log('おめでとうございます');
+    }else{
+      console.log('修正して再提出してください');
+    }
+  }else{
+    console.log('不合格です');
+    console.log('残念でした');
+  }
+}
 
+passCheck(78);
+>> 不合格です
+>> 残念でした
+
+passCheck(84);
+>> 修正して再提出してください
+
+passCheck(98);
+>> 合格です
+>> おめでとうございます
 });
