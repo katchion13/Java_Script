e555bcdfd762543b2d32f289fe88d37ddcaeb0a3
Katia Midori <69910600+katchion13@users.noreply.github.com>
2021-07-12 01:05:08 +0900

    Update script_calc.js

--- Japanese_Calendar01/script_calc.js
+++ Japanese_Calendar01/script_calc.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(){ // aguarda o DOM carregar
 
-   var sel = document.querySelector("select[name='papel']"); // seleciona o SELECT
-   var qtd = document.querySelector("input[name='quantidade']"); // seleciona o INPUT quantidade
+   var ano= document.querySelector("select[name='ano']"); // seleciona o SELECT
+   var anojp = document.querySelector("input[name='anojp']"); // seleciona o INPUT quantidade
    sel.onchange = multi; // evento change do SELECT
    qtd.oninput = multi; // evento oninput do INPUT quantidade
 
    function multi(){
-      var papel = sel.value;
-      var quant = qtd.value; // valor do INPUT quantidade
-      var res = papel * quant; // faz a multiplicação
+      var ano_cal = ano.value;
+      var anojp_cal = anojp.value; // valor do INPUT quantidade
+      var res = ano_cal * anojp_cal; // faz a multiplicação
       document.querySelector("input[name='valor']").value = !isNaN(res) ? res.toFixed(0).replace() : 0;
    }
 
